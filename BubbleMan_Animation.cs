21650ee6926a10b109194b07634a08c6b1d32e10
KyukyokuNetsu <39248807+KyukyokuNetsu@users.noreply.github.com>
2018-05-14 10:51:48 -0400

    Add files via upload

---
+++ BubbleMan_Animation.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleMan_Animation : MonoBehaviour {
+
+    //*
+    //    Devloper Name: Malik Smith
+    //    Contribution: Created Script
+    //    Bubble Man will be the only Robot Master to have slow jump animations when Mega Man enters the room
+    //    May 13, 2018
+    //*/
+
+    public GameObject metalMan;
+    private Animator jumpAnim;
+    private bool Toggle = false;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        jumpAnim = metalMan.GetComponent<Animator>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            anim.SetBool("isAttacked", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            anim.SetBool("isAttacked", false);
+        }
+
+        ToggleJumpAnim();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D entity)
+    {
+        if (entity.name.Contains("MegaMan"))
+        {
+            Toggle = true;
+            ;
+        }
+    }
+
+
+    void ToggleJumpAnim()
+    {
+        if (Toggle == true)
+        {
+            if (jumpAnim.GetBool("rightTrigger") == false)
+            {
+                jumpAnim.SetBool("rightTrigger", true);
+            }
+            else
+            {
+                jumpAnim.SetBool("rightTrigger", false);
+            }
+
+            if (jumpAnim.GetBool("leftTrigger") == false)
+            {
+                jumpAnim.SetBool("leftTrigger", true);
+            }
+            else
+            {
+                jumpAnim.SetBool("leftTrigger", false);
+            }
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D entity)
+    {
+        if (entity.name.Contains("MegaMan"))
+        {
+            Toggle = false;
+            jumpAnim.SetBool("rightTrigger", false);
+
+            Toggle = false;
+            jumpAnim.SetBool("leftTrigger", false);
+        }
+    }
+}
---
+++ MS_RobotMaster_AI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MS_RobotMaster_AI : MonoBehaviour {
+
+    //*
+    //    Devloper Name: Malik Smith
+    //    Contribution: Created Script
+    //    Robot Masters will have a health bar and damage calculation along with shooting properly
+    //    May 13, 2018
+    //*/
+
+    public int health = 10;
+	
+	public void Damage(int dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+
+    public Direction bulletDirection = Direction.RIGHT;
+    public float speed = 5.0f;
+    public int damage = 5;
+
+    private Transform _transform;
+
+        void Start() {
+        _transform = transform;
+    }
+    // Update is called once per frame
+    void Update() {
+        MoveBullet();
+    }
+
+    void MoveBullet()
+    {
+        int moveDirection = bulletDirection == Direction.LEFT ? -1 : 1;
+
+        float translate = moveDirection * speed * Time.deltaTime;
+        _transform.Translate(translate, 0, 0);
+    }
+
+    void OncollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Enemy")
+        {
+            collision.collider.gameObject.GetComponent<Enemy>().Damage(damage);
+        }
+    }
+}
